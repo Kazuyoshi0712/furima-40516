74bda4c4177f6eab2e934f62cb56c2cdb59eed81
Kazuyoshi0712 <kz.sugarsugar@gmail.com>
2024-08-01 16:25:02 +0900

    ローカルホスト　ページ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  root 'items#index'
+  resources :items
   # Defines the root path route ("/")
   # root "articles#index"
 end
