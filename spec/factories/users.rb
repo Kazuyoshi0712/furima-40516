69c0691dd2eed5756a56e2c429c552aac1ac3bc5
Kazuyoshi0712 <kz.sugarsugar@gmail.com>
2024-08-26 15:39:27 +0900

    修正１

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.last_name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { Faker::Internet.password(min_length: 6) + '1a' }
     password_confirmation { password }
   end
 end
