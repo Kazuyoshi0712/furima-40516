5b0ec81b0ce3cd768033d4c2a1327c26147fb37b
Kazuyoshi0712 <kz.sugarsugar@gmail.com>
2024-08-27 14:24:36 +0900

    修正６

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,5 @@ class ApplicationController < ActionController::Base
     keys = [:email, :password, :password_confirmation, :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana,
             :birth_date]
     devise_parameter_sanitizer.permit(:sign_up, keys:)
-    devise_parameter_sanitizer.permit(:account_update, keys:)
   end
 end
