5f23d66399557156503f04909668990f2ae2506f
Kazuyoshi0712 <kz.sugarsugar@gmail.com>
2024-08-27 11:03:51 +0900

    修正５

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角文字を使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角文字を使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'はカタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'はカタカナで入力してください' }
   validates :birth_date, presence: true
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字で入力してください' }
 end
