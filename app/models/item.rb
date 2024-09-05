39ec20a5d17f0b004d8f4986790dd681bbac10dc
Kazuyoshi0712 <kz.sugarsugar@gmail.com>
2024-09-05 16:09:32 +0900

    テストコード記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_fee
