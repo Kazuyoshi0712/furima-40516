fb7fedd2fb1e961a78074f5953cda6fd0aff5d07
Kazuyoshi0712 <kz.sugarsugar@gmail.com>
2024-09-12 23:09:27 +0900

    修正１

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :categories, only: [:show]
 
-  resources :items do
-    resources :purchases, only: [:index, :create, :new, :show]
-  end
+  #resources :items do
+   # resources :purchases, only: [:index, :create, :new, :show]
+  #end
   # Defines the root path route ("/")
   # root "articles#index"
 end
